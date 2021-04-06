b857290da518a81ba0eca02fe0e5a37822443402
neosungsin <neosungsin@aistudio.co.kr>
2021-04-06 13:03:25 +0900

    React 니꼬샘책 Chap03 까지 실습완료 배운것: props, components JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// props 에 전달되는 인자가 많을경우, ES6의 구조체 분해해서 사용해도 됨
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>
 }
 
-export default App;
+
+// function Food(props) {
+//   // console.log(props);
+//   return <h3> I love {props.fav} </h3>;
+// }
+
+// App component
+function App() {
+  // return <div className="App" />;
+  // App component 는 HTML 을 반
+  return <div>
+    <h1> Hello </h1>
+    {/*<Food fav="kimchi" something={true} papapa={['hello', 1,2,3,4,true]}/>*/}
+    <Food fav={"kimchi"} />
+    <Food fav={"ramen"} />
+    <Food fav={"samgiopsal"} />
+    <Food fav={"chukumi"} />
+  </div>;
+}
+
+export default App; // export 를 하면 다른 파일에서 Potato 컴포넌트를 사용할 수 있음 , Go 랑 비슷하네?
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.rendor() 를 통해서 App.js 에 있는 <div> 를 index.html 에 넣어줌
+// React app 은 처음부터 모든 HTML 을 그려넣진 않음. 일부 HTML 만 그리고 이후 엘리먼트를 추가하거나 제거하는 방식으로 화면을 그림
+// React keypoints: 컴포넌트, JSX, props
+// <App /> component import
+// ReactDOM.rendor(첫번재, 두번째), 첫번째에는 컴포넌트를 임포트, 두번째는 임포트한 컴포넌트가 그려질 위치
+// 리액트는 컴포넌트와 함께 동작하고, 리액트 앱은 모두 컴포넌트로 구성됨
+// 컴포넌트는 자바스크립트와 HTML 을 조합한 JSX 라는 문법을 사용해서 만듬
+ReactDOM.render( <App />,  document.getElementById('root') );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// 리엑트는 최종으로 단 한개의 컴포넌트를 그려야 하는데 두개를 그릴 경우 에러발생, 따라서 최종으로 그릴 컴포넌트 안에 컴포넌트를 추가해줘야함 !!
+// ReactDOM.render( <App /><Potato /> ,  document.getElementById('root') );
+
+// props 는 컴포넌트에서 컴포넌트로 전달하는 데이터를 의미 , 컴포넌트를 효율적으로 재사용할 수 있음
+// props 에는 불리언값 숫자, 배열 같은 다양한 데이터를 담을수있음. 문자열인 경우를 제외하면 모두 중괄호로 값을 감싸야함
+// props 는 매개변수같은 느낌으로 이해해주면 됨
